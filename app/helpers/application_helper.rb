75a4874399d280fab3800327e558ea9e443d0dff
Timur <sayreks777@gmail.com>
2017-11-16 15:11:48 +0200

    Refactor code for better understanding. Change detecting abnormal days by sum of error and failed statuses. Fix date ordering.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,14 +2,14 @@ module ApplicationHelper
 
   def abnormal_days(sessions)
     service = AnomalyDetectService.new(sessions)
-    service.get_abnormal_dates(:error) + service.get_abnormal_dates(:failed)
+    service.get_abnormal_dates
   end
 
   def status_chart_data(sessions)
-    sessions.group([:summary_status, 'date(created_at)']).order('date(created_at)').count
+    sessions.group(:summary_status, 'date(created_at)').order('date(created_at)').count
   end
 
   def duration_chart_data(sessions)
-    data = sessions.map{|x| [x.created_at.strftime('%a, %d %b %Y %H:%M:%S'), x.duration]}
+    data = sessions.map{|session| [session.created_at.strftime('%a, %d %b %Y %H:%M:%S'), session.duration]}
   end
 end
--- app/services/anomaly_detect_service.rb
+++ app/services/anomaly_detect_service.rb
@@ -3,39 +3,40 @@ class AnomalyDetectService
     @sessions = sessions
   end
 
-  def get_abnormal_dates(status)
+  def get_abnormal_dates
     # Get all failed times to detect anomalies
-    dates = get_outliers(statuses_per_date(status)).keys
-    dates.map{|x| x.strftime('%Y-%m-%d')} # format for JS lib
+    statuses = [:error, :failed]
+    dates = get_outliers(get_statuses_per_date(statuses)).keys
+    dates
   end
 
   private
 
-  def get_failed_counts
+  def get_status_counts
     data = sessions.group('date(created_at)', :summary_status).order('date(created_at)').count
-    counts = {}
     # Get formatted hash for easier analyzing
-    data.each do |k,v|
-      old_value = counts[k.first]
-      new_value = {"#{k.last}": v}
-      counts[k.first] = old_value.present? ? old_value.merge(new_value) : new_value # add new status if date already has another one
+    counts = data.each_with_object({}) do |(date_status, count), counts|
+      old_value = counts[date_status.first]
+      new_value = {"#{date_status.last}": count}
+      counts[date_status.first] = old_value.present? ? old_value.merge(new_value) : new_value # add new status if date already has another one
     end
     counts
   end
 
   # Method for grouping statuses count by date
-  def statuses_per_date(status)
-    data = {}
-    get_failed_counts.each{|k,v| data[k] = v[status].to_i}
+  def get_statuses_per_date(statuses)
+    data = get_status_counts.each_with_object({}) do |(date, counts), data|
+      data[date] = counts.select{|status| statuses.include?(status)}.values.sum # get sum count of statuses
+    end
     data
   end
 
   # Method which return keys with outlier values from hash values. 
-  def get_outliers(data)
-    values = data.values
-    mean = (values.sum.to_f / values.length)
-    standard_deviation = Math.sqrt((1 / values.length.to_f * values.inject(0){|sum, x| sum + (x - mean)**2 }))
-    data.keep_if{|k, v| (mean - v).abs > (2 * standard_deviation)} 
+  def get_outliers(dates_statuses_count)
+    counts = dates_statuses_count.values
+    mean = (counts.sum.to_f / counts.length)
+    standard_deviation = Math.sqrt((1 / counts.length.to_f * counts.inject(0){|sum, count| sum + (count - mean)**2 }))
+    dates_statuses_count.keep_if{|date, count| (mean - count).abs > (2 * standard_deviation)} 
   end
   
   attr_reader :sessions
