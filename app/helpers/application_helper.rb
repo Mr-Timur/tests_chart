d426acef745353c00b26a0dbffc90abb44b8d786
Timur <sayreks777@gmail.com>
2017-11-16 16:14:47 +0200

    Change selecting data for column stacked chart for correct displaying and ordering.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,21 @@ module ApplicationHelper
   end
 
   def status_chart_data(sessions)
-    sessions.group(:summary_status, 'date(created_at)').order('date(created_at)').count
+    service = AnomalyDetectService.new(sessions)
+    data = service.get_status_counts
+    # Get all possible statuses
+    statuses = Session.select(:summary_status).uniq.map(&:summary_status)
+    # Zeroing missing statuses for correct displaying
+    data.each do |date, statuses_count|
+      statuses.each do |status|
+        statuses_count[status.to_sym] = 0 unless statuses_count[status.to_sym].present?
+      end
+      data[date] = statuses_count
+    end
+    data.each_with_object({}){|(date, statuses_count), hash| statuses_count.each{|status, count| hash[[status.to_s, date]] = count}}
   end
 
   def duration_chart_data(sessions)
-    data = sessions.map{|session| [session.created_at.strftime('%a, %d %b %Y %H:%M:%S'), session.duration]}
+    sessions.map{|session| [session.created_at.strftime('%a, %d %b %Y %H:%M:%S'), session.duration]}
   end
 end
